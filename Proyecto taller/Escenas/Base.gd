4c7d1a61a21d6f836722014cadbbe6c2073a69cb
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-04-07 19:29:41 -0400

    Jugador ahora es rigidbody

--- Proyecto taller/Escenas/Base.gd
+++ Proyecto taller/Escenas/Base.gd
@@ -1,11 +1,11 @@
 extends RigidBody2D
 
+func _physics_process(delta):
+	if Input.is_key_pressed(KEY_D):
+		set_linear_velocity(Vector2(200, 0))
+	elif Input.is_key_pressed(KEY_A):
+		set_linear_velocity(Vector2(-200, 0))
+	else: 
+		set_linear_velocity(Vector2())
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
