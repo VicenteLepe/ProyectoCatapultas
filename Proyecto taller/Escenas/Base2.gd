1892ae03a086c3c035a0e4b3f8ce5049a9a87595
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-04-07 20:36:59 -0400

    Se corrige movimiento y se agrega jugador 2

--- Proyecto taller/Escenas/Base2.gd
+++ Proyecto taller/Escenas/Base2.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
 func _physics_process(_delta):
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(KEY_RIGHT):
 		set_linear_velocity(Vector2(150, get_linear_velocity().y))
-	elif Input.is_key_pressed(KEY_A):
+	elif Input.is_key_pressed(KEY_LEFT):
 		set_linear_velocity(Vector2(-150, get_linear_velocity().y))
 	else:
 		set_linear_velocity(Vector2((get_linear_velocity().x - get_linear_velocity().x * 0.05), get_linear_velocity().y))
