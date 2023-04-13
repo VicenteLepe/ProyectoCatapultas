27d9a10485c8cfd0a1ea883b6f85a30547edf60e
caetanossandon <caetanossandon@gmail.com>
2023-04-13 01:14:38 -0400

    Avance mecánicas de construcción
    Agregado el archivo building_plank.tscn, y un tileset para las tablas, sin embargo la funcionalidad del tileset aún no está implementado en la creación de tablas en building_plank.tscn. Actualmente solo es capaz de crear tablas a partir de dos clicks del usuario

---
+++ Proyecto taller/Escenas/Building_Scene.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+#func _input(event):
+#	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+#		var building_plank_scene = load("res://Escenas/building_plank.tscn")
+#		var plank = building_plank_scene.instantiate()
+#		plank.position = get_viewport().get_mouse_position()
+#		add_child(plank)
+var building_plank_scene = preload("res://Escenas/building_plank.tscn")
+var start_pos = null
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if start_pos == null:
+				# first click, record start position
+				start_pos = get_global_mouse_position()
+				print("Planca inicio:", get_global_mouse_position())
+			else:
+				# second click, create construction plank
+				var end_pos = get_global_mouse_position()
+				var building_plank = building_plank_scene.instantiate()
+				print("Planca final:", get_global_mouse_position())
+				building_plank.position = (start_pos + end_pos) / 2
+				building_plank.rotation = (end_pos - start_pos).angle()
+				var rigidbody = building_plank.get_node_or_null("RigidBody2D")
+				if rigidbody:
+					var collision_shape = rigidbody.get_node_or_null("CollisionShape2D")
+					if collision_shape:
+						var length = (end_pos - start_pos).length()
+						var collision_shape_length = collision_shape.shape.extents.x * 2
+						var scale_factor = length / collision_shape_length
+						collision_shape.scale.x = scale_factor
+						var sprite = rigidbody.get_child(1) # assuming sprite is the second child
+						if sprite:
+							sprite.scale.x = scale_factor
+				add_child(building_plank)
+				start_pos = null
