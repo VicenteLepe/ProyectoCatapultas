313c1f9f74240df67e421278b7d81accb975bc91
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-04-16 21:30:35 -0400

    add credits

--- Proyecto taller/Escenas/menu_inicial.gd
+++ Proyecto taller/Escenas/menu_inicial.gd
@@ -2,12 +2,17 @@ extends MarginContainer
 
 @onready var boton_start = %BotonStart
 @onready var boton_exit = %BotonExit
+@onready var boton_credits = %Credits
 @export var main_scene: PackedScene
+
 func _ready():
 	boton_start.pressed.connect(_on_start_pressed)
 	boton_exit.pressed.connect(_on_exit_pressed)
+	boton_credits.pressed.connect(_on_credits_pressed)
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(main_scene)
 	
 func _on_exit_pressed():
 	get_tree().quit()
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://Escenas/credits.tscn")
