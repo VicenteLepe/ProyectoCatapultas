7cd74b9e9df4cda1d2bce10f810b741d2f03f75b
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-04-17 09:49:55 -0400

    Se agrega escena para que construya el jugador

--- Proyecto taller/Escenas/menu_inicial.gd
+++ Proyecto taller/Escenas/menu_inicial.gd
@@ -3,14 +3,14 @@ extends MarginContainer
 @onready var boton_start = %BotonStart
 @onready var boton_exit = %BotonExit
 @onready var boton_credits = %Credits
-@export var main_scene: PackedScene
+
 
 func _ready():
 	boton_start.pressed.connect(_on_start_pressed)
 	boton_exit.pressed.connect(_on_exit_pressed)
 	boton_credits.pressed.connect(_on_credits_pressed)
 func _on_start_pressed():
-	get_tree().change_scene_to_packed(main_scene)
+	get_tree().change_scene_to_file("res://Escenas/PlayerBuilding.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit()
