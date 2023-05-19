208e883cd416e59cdffd13081275aaec02a02119
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-05-19 09:05:10 -0400

    Arreglo de la camara main scene

--- Proyecto taller/Escenas/MainScene/Camera2D.gd
+++ Proyecto taller/Escenas/MainScene/Camera2D.gd
@@ -4,3 +4,6 @@ func _physics_process(delta):
 	if not Game.player_A or not Game.player_B:
 		return
 	global_position = (Game.player_A.global_position + Game.player_B.global_position)/2
+	var distance = Game.player_A.global_position.distance_to(Game.player_B.global_position)
+	var initial_distance = 900
+	zoom = Vector2.ONE * 0.5 * initial_distance/distance
--- Proyecto taller/Escenas/PlayerScenes/Base.gd
+++ Proyecto taller/Escenas/PlayerScenes/Base.gd
@@ -6,9 +6,10 @@ func _physics_process(_delta):
 	if player == "":
 		return
 	var move_right = "move_right_"+player
+	var move_left = "move_left_"+player
 	if Input.is_action_pressed(move_right):
 		set_linear_velocity(Vector2(150, get_linear_velocity().y))
-	elif Input.is_key_pressed(KEY_A):
+	elif Input.is_action_pressed(move_left):
 		set_linear_velocity(Vector2(-150, get_linear_velocity().y))
 	else:
 		set_linear_velocity(Vector2((get_linear_velocity().x - get_linear_velocity().x * 0.05), get_linear_velocity().y))
