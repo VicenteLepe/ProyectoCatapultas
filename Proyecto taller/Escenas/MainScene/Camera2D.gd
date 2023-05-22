7b62490c36c78bcc20858c1e8037523c48ee993f
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-05-22 09:52:57 -0400

    parentesis

--- Proyecto taller/Escenas/MainScene/Camera2D.gd
+++ Proyecto taller/Escenas/MainScene/Camera2D.gd
@@ -6,7 +6,7 @@ func _physics_process(delta):
 	global_position = (Game.player_A.global_position + Game.player_B.global_position)/2
 	var distance = Game.player_A.global_position.distance_to(Game.player_B.global_position)
 	var initial_distance = 900
-	if (Vector2.ONE * 0.5 * initial_distance/distance) < Vector2.ONE * 0.5:
+	if (Vector2.ONE * 0.5 * initial_distance/distance) < (Vector2.ONE * 0.5):
 		zoom = Vector2.ONE * 0.5 * initial_distance/distance
 	else:
 		zoom = Vector2.ONE * 0.5
