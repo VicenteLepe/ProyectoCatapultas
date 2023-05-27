de40950589d9f261da2295cc82188f989f814f31
m4tu987 <m.maturana1999@gmail.com>
2023-05-27 16:16:28 -0400

    se borro codigo de boton continuar ya que no importaba

--- Proyecto taller/Escenas/BuildingScenes/Button.gd
+++ Proyecto taller/Escenas/BuildingScenes/Button.gd
@@ -1,8 +1,1 @@
-extends Button
-@onready var continuar = $"."
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-		if continuar.ACTION_MODE_BUTTON_PRESS = true:
-			get_tree().change_scene_to_file("res://Escenas/MainScene/Main.tscn")
