4142e619ecdc88942241710b14474795790bf7d8
m4tu987 <m.maturana1999@gmail.com>
2023-06-25 19:24:55 -0400

    feat: hay que implementar boton information

--- Proyecto taller/Escenas/MenuScenes/menu_inicial.gd
+++ Proyecto taller/Escenas/MenuScenes/menu_inicial.gd
@@ -3,6 +3,8 @@ extends MarginContainer
 @onready var boton_start = %BotonStart
 @onready var boton_exit = %BotonExit
 @onready var boton_credits = %Credits
+@onready var boton_information = %Information
+
 
 
 func _ready():
@@ -10,10 +12,14 @@ func _ready():
 	boton_start.pressed.connect(_on_start_pressed)
 	boton_exit.pressed.connect(_on_exit_pressed)
 	boton_credits.pressed.connect(_on_credits_pressed)
+	boton_information.pressed.connect(_on_information_pressed)
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Escenas/BuildingScenes/PlayerBuilding.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit()
+	
+func _on_information_pressed():
+	print("hay que implementar esta escena")
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://Escenas/MenuScenes/credits.tscn")
