eb06deb0ac533b21a23c2c9d9166ae22105879d8
m4tu987 <m.maturana1999@gmail.com>
2023-06-26 22:43:50 -0400

    fix: nombre boton

--- Proyecto taller/Escenas/MenuScenes/menu_inicial.gd
+++ Proyecto taller/Escenas/MenuScenes/menu_inicial.gd
@@ -3,7 +3,7 @@ extends MarginContainer
 @onready var boton_start = %BotonStart
 @onready var boton_exit = %BotonExit
 @onready var boton_credits = %Credits
-@onready var boton_information = %Information
+@onready var boton_how_to_play = %"How-to-play"
 
 
 
@@ -12,14 +12,14 @@ func _ready():
 	boton_start.pressed.connect(_on_start_pressed)
 	boton_exit.pressed.connect(_on_exit_pressed)
 	boton_credits.pressed.connect(_on_credits_pressed)
-	boton_information.pressed.connect(_on_information_pressed)
+	boton_how_to_play.pressed.connect(_on_how_to_play_pressed)
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Escenas/BuildingScenes/PlayerBuilding.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit()
 	
-func _on_information_pressed():
+func _on_how_to_play_pressed():
 	print("hay que implementar esta escena")
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://Escenas/MenuScenes/credits.tscn")
