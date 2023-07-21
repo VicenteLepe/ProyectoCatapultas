febb2980e5afe2545983aa5d118030801f13baff
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-07-21 01:09:11 -0400

    Autobuilder RopeLine

--- Proyecto taller/Escenas/construction_test.gd
+++ Proyecto taller/Escenas/construction_test.gd
@@ -93,14 +93,14 @@ func rope_shape(_end_pos, _start_pos, building_rope, object1, object2):
 	join.node_a = object1.get_path()
 	join.node_b = object2.get_path()
 	
-
-	# calculate the scale factor
-	var length = (_end_pos - _start_pos).length()
-	#var collision_shape_length = _collision_shape.shape.extents.x * 2
-	#var scale_factor = length / collision_shape_length
-	# scale the length of the plank
-	#_collision_shape.shape.extents.x = length/2 
-	#_sprite.scale.x = scale_factor
+	var line = building_rope.get_node_or_null("LineRope")
+	var node_a = Node2D.new()
+	object2.add_child(node_a)
+	node_a.global_position = _start_pos + global_position
+	var node_b = Node2D.new()
+	object1.add_child(node_b)
+	node_b.global_position = _end_pos + global_position
+	line.setup(node_a, node_b)
 	
 	building_rope.get_child(0).player = Game.player
 
