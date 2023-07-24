5df2bda3df558b4618759f667894df299865ead3
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2023-07-24 19:53:02 -0400

    fix: barras de vida

--- Proyecto taller/Escenas/MainScene/Jugador_1_vida.gd
+++ Proyecto taller/Escenas/MainScene/Jugador_1_vida.gd
@@ -3,24 +3,21 @@ extends MarginContainer
 @onready var jugador_1_vida = $"."
 @onready var progress_bar = $ProgressBar
 @onready var victory_menu_2 = $"../../CanvasLayer2/Victory_menu2"
+@onready var camera = $".."
+@onready var autobuilder = $"../../Autobuilder"
+
 
 func _physics_process(delta):
 	
-	global_position = (Game.cam_A.global_position + Game.cam_B.global_position)/2
-	var distance = Game.cam_A.global_position.distance_to(Game.cam_B.global_position)
-	var initial_distance = 900
-	if (Vector2.ONE * 0.5 * initial_distance/distance) < (Vector2.ONE * 0.5):
-		jugador_1_vida.position = Vector2(-630,-420) + Vector2.ONE * initial_distance/distance
-	else:
-		jugador_1_vida.position = Vector2(-630,-420)
-	
-	
-	
+	jugador_1_vida.position = Vector2(-610,-420) - Vector2(1200,1400) * (1/(camera.zoom.x *4)-0.5)
+	scale = Vector2(1.5, 1.5) + Vector2(2.75, 2.75) * (1/(camera.zoom.x *4)-0.5)
+
 	progress_bar.value = Game.player_1_health
 	
 	if Game.player_1_health<=0 :
 		await get_tree().create_timer(0.5).timeout
 		victory_menu_2.visible =true
+		print(autobuilder.intersections)
 	
 
 
